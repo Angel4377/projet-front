8d5bbb988781956c62053eca27c6ba30d88e0dc3
Angel4377 <angelfaye592@gmail.com>
2026-01-29 15:04:00 +0000

    Initial React front project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const students = ["Ali", "Awa", "Moussa", "Fatou"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Projet Front - DevOps</h1>
+      <p>Liste des étudiants :</p>
+
+      <ul>
+        {students.map((student, index) => (
+          <li key={index}>{student}</li>
+        ))}
+      </ul>
+
+      <button>Ajouter étudiant</button>
     </div>
   );
 }
